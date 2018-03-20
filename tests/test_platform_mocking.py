c1aac5906e82ba8ba9a4608a6282c0e6b7980995
Tomasz Kalata <klingaroo@gmail.com>
2018-03-20 16:33:58 -0400

    get_token_exception mocked

--- tests/test_platform_mocking.py
+++ tests/test_platform_mocking.py
@@ -4,7 +4,7 @@ import unittest
 from datetime import datetime, timedelta
 from mock import Mock, patch
 
-
+from context import errors
 from context import AuthorizeAccess, PlatformSession
 from context import setup_dirs
 
@@ -17,6 +17,12 @@ class TestAuthorizeAccessMock(unittest.TestCase):
         self.oauth_server = 'https://isso.nypl.org'
         self.auth = AuthorizeAccess(
             self.client_id, self.client_secret, self.oauth_server)
+        self.r = {
+            'access_token': '123abcd',
+            'token_type': 'Bearer',
+            'expires_in': 3600,
+            'id_token': '123abcd',
+            'scope': 'offline_access openid api read:bib read:item'}
 
     def test_auth_class_initialization(self):
         self.assertEquals(self.auth.client_id, 'johndoe')
@@ -29,17 +35,27 @@ class TestAuthorizeAccessMock(unittest.TestCase):
 
     @patch('overload.connectors.platform.requests.post')
     def test_get_token_header(self, mock_post):
-        r = {'access_token': '123abcd',
-             'token_type': 'Bearer',
-             'expires_in': 3600,
-             'id_token': '123abcd',
-             'scope': 'offline_access openid api read:bib read:item'}
 
         mock_post.return_value = Mock(status_code=200)
-        mock_post.return_value.json.return_value = r
+        mock_post.return_value.json.return_value = self.r
+        token = self.auth.get_token()
 
-        # self.assertIn(self.auth.get_token())
-        self.assertEqual(self.auth.get_token()['id'], '123abcd')
+        self.assertIn('id', token)
+        self.assertIn('expires_on', token)
+        self.assertEqual(token.get('id'), '123abcd')
+        expires_on = datetime.now() + timedelta(seconds=3600 - 1)
+        self.assertEqual(token.get('expires_on'), expires_on)
+
+    @patch('overload.connectors.platform.requests.post')
+    def test_get_token_exceptions(self, mock_post):
+        e = {'error': 'error_type'}
+        mock_post.return_value = Mock(status_code=401)
+        mock_post.return_value.json.return_value = e
+        with self.assertRaises(errors.APITokenError):
+            self.auth.get_token()
+
+
+
 
 
 if __name__ == '__main__':
