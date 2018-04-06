079d6679c9ec1adc9c183d8ecc511255bd6fb4fb
Tomasz Kalata <klingaroo@gmail.com>
2018-04-06 16:21:46 -0400

    cat workflow completed

---
+++ overload/pvf/analyzer.py
@@ -0,0 +1,211 @@
+# compares vendor bib meta and inhouse bib meta and returns analysis object
+
+
+class PVRReport:
+    """
+    A general analysis report class.
+    Use analysis classes specific to each system.
+    """
+    def __init__(self, meta_vendor, meta_inhouse):
+        self._meta_vendor = meta_vendor
+        self._meta_inhouse = meta_inhouse
+        self.vendor_resource_id = None
+        self.vendor = meta_vendor.vendor
+        self.updated_by_vendor = False
+        self.callNo_match = False
+        self.vendor_callNo = meta_vendor.bCallNumber
+        self.target_sierraId = None
+        self.target_callNo = None
+        self.inhouse_dups = []
+        self.action = 'insert'
+
+        self._determine_resource_id()
+        self._determine_vendor_callNo()
+        self._order_inhouse_meta()
+
+    def _determine_resource_id(self):
+        if self._meta_vendor.t001 is not None:
+            self.vendor_resource_id = self._meta_vendor.t001
+        elif self._meta_vendor.t020 != []:
+            self.vendor_resource_id = self._meta_vendor.t020[0]
+        elif self._meta_vendor.t024 != []:
+            self.vendor_resource_id = self._meta_vendor.t024[0]
+
+    def _determine_vendor_callNo(self):
+        if self._meta_vendor.dstLibrary == 'branches':
+            self.vendor_callNo = self._meta_vendor.bCallNumber
+        elif self._meta_vendor.dstLibrary == 'research':
+            try:
+                self.vendor_callNo = self._meta_vendor.rCallNumber[0]
+            except IndexError:
+                self.vendor_callNo = None
+
+    def _order_inhouse_meta(self):
+        # will remove any duplicates from the Sierra results
+        # order from the newest bib to the oldest
+        descending_order = []
+        bib_order = dict()
+        n = -1
+        for meta in self._meta_inhouse:
+            n += 1
+            bib_order[meta.sierraId] = n
+
+        for key, value in sorted(bib_order.iteritems()):
+            descending_order.append(self._meta_inhouse[value])
+        self._meta_inhouse = descending_order
+
+    def _compare_update_timestamp(self, meta_inhouse):
+        updated = False
+        if self._meta_vendor.t005 is None:
+            pass
+        elif meta_inhouse.t005 is None:
+            updated = True
+        elif self._meta_vendor.t005 > meta_inhouse.t005:
+            updated = True
+        return updated
+
+    def _determine_callNo_match(self, meta_inhouse):
+        match = False
+        if self._meta_vendor.bCallNumber is None or \
+                meta_inhouse.bCallNumber is None:
+            match = True
+        elif self._meta_vendor.bCallNumber == \
+                meta_inhouse.bCallNumber:
+            match = True
+        return match
+
+
+class PVR_NYPLReport(PVRReport):
+    """
+    Creates a NYPL analysis report of a vendor record in relation
+    to retrieved from the catalog existing matching bibs
+    """
+    def __init__(self, agent, meta_vendor, meta_inhouse):
+        PVRReport.__init__(self, meta_vendor, meta_inhouse)
+        self._matched = []
+        self.mixed = []
+        self.other = []
+
+        self._group_by_library()
+
+        if agent == 'cat':
+            self._cataloging_workflow()
+        elif agent == 'sel':
+            self._selection_workflow()
+        elif agent == 'acq':
+            self._acquisition_workflow()
+
+    def _group_by_library(self):
+
+        for meta in self._meta_inhouse:
+            # print self._meta_vendor.dstLibrary, print self.li
+            if self._meta_vendor.dstLibrary == meta.ownLibrary:
+                # correct library
+                self._matched.append(meta)
+            elif meta.ownLibrary == 'mixed':
+                self.mixed.append(meta.sierraId)
+            else:
+                self.other.append(meta.sierraId)
+
+    def _cataloging_workflow(self):
+        # default action = 'insert'
+        n = len(self._matched)
+        if n > 0:
+            c = 0
+            for meta in self._matched:
+                c += 1
+                if meta.bCallNumber is not None or \
+                        len(meta.rCallNumber) > 0:
+                    # full record
+                    if self._meta_vendor.dstLibrary == 'branches':
+                        # check if call number matches
+                        call_match = self._determine_callNo_match(meta)
+                        if call_match:
+                            self.callNo_match = True
+                            self.target_sierraId = meta.sierraId
+                            self.target_callNo = meta.bCallNumber
+                            if meta.catSource == 'inhouse':
+                                self.action = 'attach'
+                            else:
+                                updated = self._compare_update_timestamp(meta)
+                                if updated:
+                                    self.updated_by_vendor = True
+                                    self.action = 'overlay'
+                                else:
+                                    self.action = 'attach'
+                            # do not check any further
+                            break
+                        else:
+                            if c == n:
+                                self.target_sierraId = meta.sierraId
+                                self.target_callNo = meta.bCallNumber
+                                if meta.catSource == 'inhouse':
+                                    self.action = 'attach'
+                                else:
+                                    updated = self._compare_update_timestamp(
+                                        meta)
+                                    if updated:
+                                        self.updated_by_vendor = True
+                                        self.action = 'overlay'
+                                    else:
+                                        self.action = 'attach'
+                    else:
+                        # research path, no callNo match checking
+                        self.callNo_match = True  # is it ok to do? can differences in the call numbers be ignored for research mats?
+                        self.target_sierraId = meta.sierraId
+                        self.target_callNo = ','.join(meta.rCallNumber)
+                        if meta.catSource == 'inhouse':
+                            self.action = 'attach'
+                        else:
+                            updated = self._compare_update_timestamp(meta)
+                            if updated:
+                                self.updated_by_vendor = True
+                                self.action = 'overlay'
+                            else:
+                                self.action = 'attach'
+                        break
+                else:
+                    # brief record matching dstLibrary
+                    # overlay the earliest record
+                    if c == n:
+                        # no other bibs to consider
+                        self.callNo_match = True
+                        self.target_sierraId = meta.sierraId
+                        self.action = 'overlay'
+        if n > 1:
+            self.inhouse_dups = [meta.sierraId for meta in self._matched]
+
+    def _selection_workflow(self):
+        pass
+
+    def _acquisition_workflow(self):
+        pass
+
+    def to_dict(self):
+        return {
+            'resource_id': self.vendor_resource_id,
+            'vendor': self.vendor,
+            'updated_by_vendor': self.updated_by_vendor,
+            'callNo_match': self.callNo_match,
+            'target_callNo': self.target_callNo,
+            'inhouse_dups': ','.join(self.inhouse_dups),
+            'target_sierraId': self.target_sierraId,
+            'mixed': ','.join(self.mixed),
+            'other': ','.join(self.other),
+            'action': self.action}
+
+    def __repr__(self):
+        return "<PVF Report(vendor_resource_id={}, vendor={}, " \
+            "callNo_match={}, target_callNo={}, updated_by_vendor={}, " \
+            "inhouse_dups={}, target_sierraId={}, mixed={}, "\
+            "other={}, action={})>".format(
+                self.vendor_resource_id,
+                self.vendor,
+                self.callNo_match,
+                self.target_callNo,
+                self.updated_by_vendor,
+                self.inhouse_dups,
+                self.target_sierraId,
+                self.mixed,
+                self.other,
+                self.action)
