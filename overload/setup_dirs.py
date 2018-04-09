da50f0a6129091df8f75ac45d3a8b0f8ed7357a7
Tomasz Kalata <klingaroo@gmail.com>
2018-04-09 17:12:04 -0400

    datastore dir added

--- overload/setup_dirs.py
+++ overload/setup_dirs.py
@@ -5,10 +5,9 @@ import os
 APP_DIR = os.environ['APPDATA'] + r'\Overload'
 MY_DOCS = os.path.expanduser(os.sep.join(["~", "Documents"]))
 TEMP_DIR = APP_DIR + r'\temp'
-SUMMARIES_DIR = r'S:\CATAL\BookOps Apps\Overload STATS'
 USER_DATA = APP_DIR + r'\user_data'
-USER_STATS = APP_DIR + r'\user_stats.csv'
-BATCH_STATS = TEMP_DIR + r'\session_report.db'
-VENDOR_STATS = SUMMARIES_DIR + r'\vendor_totals.csv'
+DATASTORE = APP_DIR + r'\datastore.db'  # move some user_data here
+BATCH_STATS = TEMP_DIR + r'\batch_stats'
+BATCH_META = TEMP_DIR + r'\batch_meta'
 MVAL_REP = TEMP_DIR + r'\marcedit_validation_report.txt'
 CVAL_REP = TEMP_DIR + r'\combined_validation_report.txt'
