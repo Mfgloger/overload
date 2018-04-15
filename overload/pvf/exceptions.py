1fc024511f339c5ff768a03781cea6d54a3b49f6
klinga <klingaroo@gmail.com>
2018-04-14 23:08:15 -0400

    pvr module exceptions

---
+++ overload/pvf/exceptions.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""
+overload.exceptions
+~~~~~~~~~~~~~~~~~~~
+This module contains the set of Overload's exceptions.
+"""
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
