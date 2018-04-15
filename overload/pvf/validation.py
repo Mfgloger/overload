110d8aeae47ef342a488ee6aebb5a9f9757de645
klinga <klingaroo@gmail.com>
2018-04-14 23:08:37 -0400

    add validate_files function

---
+++ overload/pvf/validation.py
@@ -0,0 +1,46 @@
+# runs MarcEdit and local validation
+
+from validators import marcedit
+from pvf.exceptions import Error
+from setup_dirs import MVAL_REP
+
+
+def validate_files(files, marcval=False, locval=False):
+    valid_files = True
+    if marcval:
+        # make sure MARCEdit is installed on the machine
+        val_engine = marcedit.get_engine()
+        if val_engine is None:
+            # display error message
+            raise Error(
+                'Failed to locate cmarcedit.exe or marcrules.txt\n'
+                'files of MARCEdit program. Unable to complete\n'
+                'MARC validation. Please uncheck the box if you\n'
+                'still want to proceed.')
+        else:
+            cme = val_engine[0]
+            rules = val_engine[1]
+            report_q = MVAL_REP
+            overwrite = True
+            for file in files:
+                file_q = file
+                success_process = marcedit.validate(
+                    cme, file_q, report_q, rules, overwrite)
+                overwrite = False
+                if success_process:
+                    result = marcedit.validation_check(MVAL_REP)
+                    if not result[0]:
+                        valid_files = False
+                else:
+                    valid_files = False
+                    raise Error(
+                        'Encounted a problem with the file:\n'
+                        '{}.\nNot able to validate in MARCEdit'.format(
+                            file))
+    if locval:
+        # placeholder; replace when local validation is developed
+        raise Error(
+            'Local specs validation is still being developed.\n'
+            'Uncheck the box to not display this warning')
+
+    return valid_files
