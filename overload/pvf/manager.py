62c1147d439d23983b497999a3acf5221a8fbfb9
Tomasz Kalata <klingaroo@gmail.com>
2018-06-25 11:50:00 -0400

    delete present sierra id if specified

--- overload/pvf/manager.py
+++ overload/pvf/manager.py
@@ -91,8 +91,46 @@ def run_processing(
     module_logger.debug(
         'Creatig vendor index data for {}-{}'.format(
             system, agent))
-    rules = './rules/vendors.xml'
-    vx = vendor_index(rules, system, agent)  # wrap in exception?
+    if agent == 'cat':
+        rules = './rules/vendors.xml'
+        vx = vendor_index(rules, system)  # wrap in exception?
+    elif agent in ('sel', 'acq'):
+        if system == 'nypl':
+            query_matchpoints = dict()
+            with session_scope() as db_session:
+                try:
+                    trec = retrieve_record(
+                        db_session, NYPLOrderTemplate, tName=template,
+                        agent=agent)
+
+                    if trec.match1st == 'sierra_id':
+                        query_matchpoints['primary'] = (
+                            'id', trec.match1st)
+                    else:
+                        query_matchpoints['primary'] = (
+                            'tag', trec.match1st)
+                    if trec.match2nd is not None:
+                        if trec.match2nd == 'sierra_id':
+                            query_matchpoints['secondary'] = (
+                                'id', trec.match2nd)
+                        else:
+                            query_matchpoints['secondary'] = (
+                                'tag', trec.match2nd)
+                    if trec.match3rd is not None:
+                        if trec.match3rd == 'sierra_id':
+                            query_matchpoints['tertiary'] = (
+                                'id', trec.match3rd)
+                        else:
+                            query_matchpoints['tertiary'] = (
+                                'tag', trec.match3rd)
+                except NoResultFound:
+                    raise OverloadError(
+                        'Unable to find template {}.\n'
+                        'Please verify it exists.'.format(
+                            template))
+        else:
+            raise OverloadError(
+                'selection workflow for BPL not implemented yet')
 
     # run queries and results analysis for each bib in each file
     n = 0
@@ -123,47 +161,12 @@ def run_processing(
                         'in cat vendor index'.format(
                             vendor))
             elif agent in ('sel', 'acq'):
+                # vendor code
                 if system == 'nypl':
-                    query_matchpoints = dict()
-                    with session_scope() as db_session:
-                        try:
-                            trec = retrieve_record(
-                                db_session, NYPLOrderTemplate, tName=template,
-                                agent=agent)
-
-                            if trec.match1st == 'sierra_id':
-                                query_matchpoints['primary'] = (
-                                    'id', trec.match1st)
-                            else:
-                                query_matchpoints['primary'] = (
-                                    'tag', trec.match1st)
-                            if trec.match2nd is not None:
-                                if trec.match2nd == 'sierra_id':
-                                    query_matchpoints['secondary'] = (
-                                        'id', trec.match2nd)
-                                else:
-                                    query_matchpoints['secondary'] = (
-                                        'tag', trec.match2nd)
-                            if trec.match3rd is not None:
-                                if trec.match3rd == 'sierra_id':
-                                    query_matchpoints['tertiary'] = (
-                                        'id', trec.match3rd)
-                                else:
-                                    query_matchpoints['tertiary'] = (
-                                        'tag', trec.match3rd)
-                        except NoResultFound:
-                            raise OverloadError(
-                                'Unable to find template {}.\n'
-                                'Please verify it exists.'.format(
-                                    template))
-                    # vendor code
                     vendor = get_vendor_from_nypl_template(template, agent)
                     if vendor is None:
                         # do not apply but keep for stats
                         vendor = 'UNKNOWN'
-                else:
-                    raise OverloadError(
-                        'selection workflow for BPL not implemented yet')
 
             if vendor == 'UNKNOWN':
                 module_logger.warning(
@@ -365,6 +368,18 @@ def run_processing(
             module_logger.info(
                 'Adding template field(s) to the vendor record.')
             if agent == 'cat':
+                # delete present sierra id if specified
+                if sierra_id_present and \
+                        vx[vendor]['existing_sierraId'] == 'remove_new' and \
+                        analysis['action'] == 'insert' and \
+                        analysis['target_sierraId'] is None:
+                    module_logger.debug(
+                        'Removing present sierra id field.')
+                    if system == 'nypl':
+                        bib.remove_fields('945')
+                    elif system == 'bpl':
+                        bib.remove_fields('907')
+
                 templates = vx[vendor].get('bib_template')
                 module_logger.debug(
                     'Selected CAT templates for {}: {}'.format(
