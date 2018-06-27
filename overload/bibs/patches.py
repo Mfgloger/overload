e7133c8b5aa1ed37766ba61b04af36ddfbce9317
Tomasz Kalata <klingaroo@gmail.com>
2018-06-27 09:48:43 -0400

    initial bug fixes

--- overload/bibs/patches.py
+++ overload/bibs/patches.py
@@ -3,12 +3,17 @@
 from pymarc import Field
 
 
-def patches(system, library, agent, vendor, bib):
+def bib_patches(system, library, agent, vendor, bib):
+    """
+    Treatment of specific records; special cases
+    """
+
     # nypl branches cat patch for BT Series
     if system == 'nypl' and library == 'branches' and \
             agent == 'cat' and vendor == 'BT SERIES':
         # parse the call number
         new_callno = []
+        pos = 0
         if '091' in bib:
             callno = bib.get_fields('091')[0].value()
 
@@ -27,7 +32,10 @@ def patches(system, library, agent, vendor, bib):
                 new_callno.extend(['f', 'YR'])
 
             # main subfield
-            if 'FIC ' in callno:
+            if 'GN FIC ' in callno:
+                pos = callno.index('GN FIC ') + 7
+                new_callno.extend(['a', 'GN FIC'])
+            elif 'FIC ' in callno:
                 pos = callno.index('FIC ') + 4
                 new_callno.extend(['a', 'FIC'])
             elif 'PIC ' in callno:
@@ -36,12 +44,9 @@ def patches(system, library, agent, vendor, bib):
             elif callno[:4] == 'J E ':
                 pos = callno.index('J E ') + 4
                 new_callno.extend(['a', 'E'])
-            elif callno[:7] == 'J SPA E':
-                pos = callno.index('J SPA E') + 7
+            elif callno[:8] == 'J SPA E ':
+                pos = callno.index('J SPA E ') + 8
                 new_callno.extend(['a', 'E'])
-            elif 'GN FIC ' in callno:
-                pos = callno.index('GN FIC ') + 7
-                new_callno.extend(['a', 'GN FIC'])
 
             # cutter subfield
             new_callno.extend(['c', callno[pos:]])
@@ -52,9 +57,12 @@ def patches(system, library, agent, vendor, bib):
 
             # verify nothing has been lost
             if callno != field.value():
-                raise AssertionError
+                raise AssertionError(
+                    'Constructed call # does not match original.'
+                    'New={}, original={}'.format(
+                        callno, field.value()))
             else:
                 # if correct remove the original and replace with new
                 bib.remove_fields('091')
                 bib.add_ordered_field(field)
-                return bib
+    return bib
--- tests/test_bib_patches.py
+++ tests/test_bib_patches.py
@@ -8,10 +8,29 @@ from context import patches
 
 
 class TestBibsPatches(unittest.TestCase):
-    def setUp(self):
-        # Test MARC record
-        self.bib1 = Record()
-        self.bib1.leader = '00000nam a2200000u  4500'
+    def test_nypl_branch_BT_SERIES_exception(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J B EDISON C']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+        with self.assertRaises(AssertionError):
+            patches.bib_patches(
+                'nypl', 'branches', 'cat', 'BT SERIES', bib)
+
+    def test_nypl_branch_BT_SERIES_JE_call_patch(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
         tags = []
         tags.append(
             Field(tag='001', data='0001'))
@@ -24,14 +43,436 @@ class TestBibsPatches(unittest.TestCase):
                   indicators=[' ', ' '],
                   subfields=['a', 'J E VAMPIRINA']))
         for tag in tags:
-            self.bib1.add_ordered_field(tag)
+            bib.add_ordered_field(tag)
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
 
-    def test_nypl_branch_BT_SERIES_patch(self):
-        bib = patches.patches(
-            'nypl', 'branches', 'cat', 'BT SERIES', self.bib1)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J',
+            'a', 'E',
+            'c', 'VAMPIRINA']
         self.assertEqual(
-            bib.get_fields('091')[0].value(),
-            'J E VAMPIRINA')
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branch_BT_SERIES_juvenile_pic_compound_name(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J E COMPOUND NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J',
+            'a', 'E',
+            'c', 'COMPOUND NAME']
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branch_BT_SERIES_Spanish_prefix(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J SPA E COMPOUND NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J SPA',
+            'a', 'E',
+            'c', 'COMPOUND NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branch_BT_SERIES_juvenile_fiction(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J FIC NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J',
+            'a', 'FIC',
+            'c', 'NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branch_BT_SERIES_juvenile_fic_compound_name(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J FIC COMPOUND NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J',
+            'a', 'FIC',
+            'c', 'COMPOUND NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branch_BT_SERIES_picture_book(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J PIC NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J',
+            'a', 'PIC',
+            'c', 'NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branches_BT_SERIES_picture_book_compound_name(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J PIC COMPOUND NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J',
+            'a', 'PIC',
+            'c', 'COMPOUND NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branches_BT_SERIES_Spanish_picture_book(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J SPA PIC NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J SPA',
+            'a', 'PIC',
+            'c', 'NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branch_BT_SERIES_young_reader(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J YR FIC NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J',
+            'f', 'YR',
+            'a', 'FIC',
+            'c', 'NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branch_BT_SERIES_young_reader_compound_name(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'J YR FIC COMPOUND NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'p', 'J',
+            'f', 'YR',
+            'a', 'FIC',
+            'c', 'COMPOUND NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branches_BT_SERIES_YA_fiction(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'FIC NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'a', 'FIC',
+            'c', 'NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branches_BT_SERIES_YA_fiction_compound_name(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'FIC COMPOUND NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'a', 'FIC',
+            'c', 'COMPOUND NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branches_BT_SERIES_YA_graphic_novel(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'GRAPHIC GN FIC NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'f', 'GRAPHIC',
+            'a', 'GN FIC',
+            'c', 'NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
+
+    def test_nypl_branches_BT_SERIES_YA_graphic_novel_compound_name(self):
+        bib = Record()
+        bib.leader = '00000nam a2200000u  4500'
+        tags = []
+        tags.append(
+            Field(tag='001', data='0001'))
+        tags.append(
+            Field(tag='245',
+                  indicators=['0', '0'],
+                  subfields=['a', 'Test title']))
+        tags.append(
+            Field(tag='091',
+                  indicators=[' ', ' '],
+                  subfields=['a', 'GRAPHIC GN FIC COMPOUND NAME']))
+        for tag in tags:
+            bib.add_ordered_field(tag)
+
+        mod_bib = patches.bib_patches(
+            'nypl', 'branches', 'cat', 'BT SERIES', bib)
+        correct_indicators = [' ', ' ']
+        correct_subfields = [
+            'f', 'GRAPHIC',
+            'a', 'GN FIC',
+            'c', 'COMPOUND NAME']
+
+        self.assertEqual(
+            correct_indicators,
+            mod_bib.get_fields('091')[0].indicators)
+        self.assertEqual(
+            correct_subfields,
+            mod_bib.get_fields('091')[0].subfields)
 
 
 if __name__ == '__main__':
