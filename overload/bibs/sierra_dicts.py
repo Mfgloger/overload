78403d6c2fd891548988c49e3ce0291a06f8483b
Tomek <klinga@users.noreply.github.com>
2018-08-01 13:08:49 -0400

    Local specs (#5)
    * formatting fix
    
    * NYPL & BPL branches dicts added
    
    * branches as dicts
    
    * typo correction

--- overload/bibs/sierra_dicts.py
+++ overload/bibs/sierra_dicts.py
@@ -28,7 +28,7 @@ NCLAIM = {
     'f': 'cl 6 dec md'
 }
 
-# nypl order code1 
+# nypl order code1
 NORDER_CODE1 = {
     'w': 'Wayne',
     'c': 'Celeste',
@@ -177,671 +177,338 @@ LANG = {
 
 COUNTRIES = {
     "aa ": "Albania",
-
     "abc": "Alberta",
-
     "aca": "Australian Capital Territory",
-
     "ae ": "Algeria",
-
     "af ": "Afghanistan",
-
     "ag ": "Argentina",
-
     "ai ": "Armenia (Republic)",
-
     "aj ": "Azerbaijan",
-
     "aku": "Alaska",
-
     "alu": "Alabama",
-
     "am ": "Anguilla",
-
     "an ": "Andorra",
-
     "ao ": "Angola",
-
     "aq ": "Antigua and Barbuda",
-
     "aru": "Arkansas",
-
     "as ": "American Samoa",
-
     "at ": "Australia",
-
     "au ": "Austria",
-
     "aw ": "Aruba",
-
     "ay ": "Antarctica",
-
     "azu": "Arizona",
-
     "ba ": "Bahrain",
-
     "bb ": "Barbados",
-
     "bcc": "British Columbia",
-
     "bd ": "Burundi",
-
     "be ": "Belgium",
-
     "bf ": "Bahamas",
-
     "bg ": "Bangladesh",
-
     "bh ": "Belize",
-
     "bi ": "British Indian Ocean Territory",
-
     "bl ": "Brazil",
-
     "bm ": "Bermuda Islands",
-
     "bn ": "Bosnia and Herzegovina",
-
     "bo ": "Bolivia",
-
     "bp ": "Solomon Islands",
-
     "br ": "Burma",
-
     "bs ": "Botswana",
-
     "bt ": "Bhutan",
-
     "bu ": "Bulgaria",
-
     "bv ": "Bouvet Island",
-
     "bw ": "Belarus",
-
     "bx ": "Brunei",
-
     "ca ": "Caribbean Netherlands",
-
     "cau": "California",
-
     "cb ": "Cambodia",
-
     "cc ": "China",
-
     "cd ": "Chad",
-
     "ce ": "Sri Lanka",
-
     "cf ": "Congo (Brazzaville)",
-
     "cg ": "Congo (Democratic Republic)",
-
     "ch ": "China (Republic : 1949- )",
-
     "ci ": "Croatia",
-
     "cj ": "Cayman Islands",
-
     "ck ": "Colombia",
-
     "cl ": "Chile",
-
     "cm ": "Cameroon",
-
     "co ": "Curaçao",
-
     "cou": "Colorado",
-
     "cq ": "Comoros",
-
     "cr ": "Costa Rica",
-
     "ctu": "Connecticut",
-
     "cu ": "Cuba",
-
     "cv ": "Cabo Verde",
-
     "cw ": "Cook Islands",
-
     "cx ": "Central African Republic",
-
     "cy ": "Cyprus",
-
     "dcu": "District of Columbia",
-
     "deu": "Delaware",
-
     "dk ": "Denmark",
-
     "dm ": "Benin",
-
     "dq ": "Dominica",
-
     "dr ": "Dominican Republic",
-
     "ea ": "Eritrea",
-
     "ec ": "Ecuador",
-
     "eg ": "Equatorial Guinea",
-
     "em ": "Timor-Leste",
-
     "enk": "England",
-
     "er ": "Estonia",
-
     "es ": "El Salvador",
-
     "et ": "Ethiopia",
-
     "fa ": "Faroe Islands",
-
     "fg ": "French Guiana",
-
     "fi ": "Finland",
-
     "fj ": "Fiji",
-
     "fk ": "Falkland Islands",
-
     "flu": "Florida",
-
     "fm ": "Micronesia (Federated States)",
-
     "fp ": "French Polynesia",
-
     "fr ": "France",
-
     "fs ": "Terres australes et antarctiques françaises",
-
     "ft ": "Djibouti",
-
     "gau": "Georgia",
-
     "gb ": "Kiribati",
-
     "gd ": "Grenada",
-
     "gg ": "Guernsey",
-
     "gh ": "Ghana",
-
     "gi ": "Gibraltar",
-
     "gl ": "Greenland",
-
     "gm ": "Gambia",
-
     "go ": "Gabon",
-
     "gp ": "Guadeloupe",
-
     "gr ": "Greece",
-
     "gs ": "Georgia (Republic)",
-
     "gt ": "Guatemala",
-
     "gu ": "Guam",
-
     "gv ": "Guinea",
-
     "gw ": "Germany",
-
     "gy ": "Guyana",
-
     "gz ": "Gaza Strip",
-
     "hiu": "Hawaii",
-
     "hm ": "Heard and McDonald Islands",
-
     "ho ": "Honduras",
-
     "ht ": "Haiti",
-
     "hu ": "Hungary",
-
     "iau": "Iowa",
-
     "ic ": "Iceland",
-
     "idu": "Idaho",
-
     "ie ": "Ireland",
-
     "ii ": "India",
-
     "ilu": "Illinois",
-
     "im ": "Isle of Man",
-
     "inu": "Indiana",
-
     "io ": "Indonesia",
-
     "iq ": "Iraq",
-
     "ir ": "Iran",
-
     "is ": "Israel",
-
     "it ": "Italy",
-
     "iv ": "Côte d'Ivoire",
-
     "iy ": "Iraq-Saudi Arabia Neutral Zone",
-
     "ja ": "Japan",
-
     "je ": "Jersey",
-
     "ji ": "Johnston Atoll",
-
     "jm ": "Jamaica",
-
     "jo ": "Jordan",
-
     "ke ": "Kenya",
-
     "kg ": "Kyrgyzstan",
-
     "kn ": "Korea (North)",
-
     "ko ": "Korea (South)",
-
     "ksu": "Kansas",
-
     "ku ": "Kuwait",
-
     "kv ": "Kosovo",
-
     "kyu": "Kentucky",
-
     "kz ": "Kazakhstan",
-
     "lau": "Louisiana",
-
     "lb ": "Liberia",
-
     "le ": "Lebanon",
-
     "lh ": "Liechtenstein",
-
     "li ": "Lithuania",
-
     "lo ": "Lesotho",
-
     "ls ": "Laos",
-
     "lu ": "Luxembourg",
-
     "lv ": "Latvia",
-
     "ly ": "Libya",
-
     "mau": "Massachusetts",
-
     "mbc": "Manitoba",
-
     "mc ": "Monaco",
-
     "mdu": "Maryland",
-
     "meu": "Maine",
-
     "mf ": "Mauritius",
-
     "mg ": "Madagascar",
-
     "miu": "Michigan",
-
     "mj ": "Montserrat",
-
     "mk ": "Oman",
-
     "ml ": "Mali",
-
     "mm ": "Malta",
-
     "mnu": "Minnesota",
-
     "mo ": "Montenegro",
-
     "mou": "Missouri",
-
     "mp ": "Mongolia",
-
     "mq ": "Martinique",
-
     "mr ": "Morocco",
-
     "msu": "Mississippi",
-
     "mtu": "Montana",
-
     "mu ": "Mauritania",
-
     "mv ": "Moldova",
-
     "mw ": "Malawi",
-
     "mx ": "Mexico",
-
     "my ": "Malaysia",
-
     "mz ": "Mozambique",
-
     "nbu": "Nebraska",
-
     "ncu": "North Carolina",
-
     "ndu": "North Dakota",
-
     "ne ": "Netherlands",
-
     "nfc": "Newfoundland and Labrador",
-
     "ng ": "Niger",
-
     "nhu": "New Hampshire",
-
     "nik": "Northern Ireland",
-
     "nju": "New Jersey",
-
     "nkc": "New Brunswick",
-
     "nl ": "New Caledonia",
-
     "nmu": "New Mexico",
-
     "nn ": "Vanuatu",
-
     "no ": "Norway",
-
     "np ": "Nepal",
-
     "nq ": "Nicaragua",
-
     "nr ": "Nigeria",
-
     "nsc": "Nova Scotia",
-
     "ntc": "Northwest Territories",
-
     "nu ": "Nauru",
-
     "nuc": "Nunavut",
-
     "nvu": "Nevada",
-
     "nw ": "Northern Mariana Islands",
-
     "nx ": "Norfolk Island",
-
     "nyu": "New York (State)",
-
     "nz ": "New Zealand",
-
     "ohu": "Ohio",
-
     "oku": "Oklahoma",
-
     "onc": "Ontario",
-
     "oru": "Oregon",
-
     "ot ": "Mayotte",
-
     "pau": "Pennsylvania",
-
     "pc ": "Pitcairn Island",
-
     "pe ": "Peru",
-
     "pf ": "Paracel Islands",
-
     "pg ": "Guinea-Bissau",
-
     "ph ": "Philippines",
-
     "pic": "Prince Edward Island",
-
     "pk ": "Pakistan",
-
     "pl ": "Poland",
-
     "pn ": "Panama",
-
     "po ": "Portugal",
-
     "pp ": "Papua New Guinea",
-
     "pr ": "Puerto Rico",
-
     "pw ": "Palau",
-
     "py ": "Paraguay",
-
     "qa ": "Qatar",
-
     "qea": "Queensland",
-
     "quc": "Québec (Province)",
-
     "rb ": "Serbia",
-
     "re ": "Réunion",
-
     "rh ": "Zimbabwe",
-
     "riu": "Rhode Island",
-
     "rm ": "Romania",
-
     "ru ": "Russia (Federation)",
-
     "rw ": "Rwanda",
-
     "sa ": "South Africa",
-
     "sc ": "Saint-Barthélemy",
-
     "scu": "South Carolina",
-
     "sd ": "South Sudan",
-
     "sdu": "South Dakota",
-
     "se ": "Seychelles",
-
     "sf ": "Sao Tome and Principe",
-
     "sg ": "Senegal",
-
     "sh ": "Spanish North Africa",
-
     "si ": "Singapore",
-
     "sj ": "Sudan",
-
     "sl ": "Sierra Leone",
-
     "sm ": "San Marino",
-
     "sn ": "Sint Maarten",
-
     "snc": "Saskatchewan",
-
     "so ": "Somalia",
-
     "sp ": "Spain",
-
     "sq ": "Swaziland",
-
     "sr ": "Surinam",
-
     "ss ": "Western Sahara",
-
     "st ": "Saint-Martin",
-
     "stk": "Scotland",
-
     "su ": "Saudi Arabia",
-
     "sw ": "Sweden",
-
     "sx ": "Namibia",
-
     "sy ": "Syria",
-
     "sz ": "Switzerland",
-
     "ta ": "Tajikistan",
-
     "tc ": "Turks and Caicos Islands",
-
     "tg ": "Togo",
-
     "th ": "Thailand",
-
     "ti ": "Tunisia",
-
     "tk ": "Turkmenistan",
-
     "tl ": "Tokelau",
-
     "tma": "Tasmania",
-
     "tnu": "Tennessee",
-
     "to ": "Tonga",
-
     "tr ": "Trinidad and Tobago",
-
     "ts ": "United Arab Emirates",
-
     "tu ": "Turkey",
-
     "tv ": "Tuvalu",
-
     "txu": "Texas",
-
     "tz ": "Tanzania",
-
     "ua ": "Egypt",
-
     "uc ": "United States Misc. Caribbean Islands",
-
     "ug ": "Uganda",
-
     "un ": "Ukraine",
-
     "up ": "United States Misc. Pacific Islands",
-
     "utu": "Utah",
-
     "uv ": "Burkina Faso",
-
     "uy ": "Uruguay",
-
     "uz ": "Uzbekistan",
-
     "vau": "Virginia",
-
     "vb ": "British Virgin Islands",
-
     "vc ": "Vatican City",
-
     "ve ": "Venezuela",
-
     "vi ": "Virgin Islands of the United States",
-
     "vm ": "Vietnam",
-
     "vp ": "Various places",
-
     "vra": "Victoria",
-
     "vtu": "Vermont",
-
     "wau": "Washington (State)",
-
     "wea": "Western Australia",
-
     "wf ": "Wallis and Futuna",
-
     "wiu": "Wisconsin",
-
     "wj ": "West Bank of the Jordan River",
-
     "wk ": "Wake Island",
-
     "wlk": "Wales",
-
     "ws ": "Samoa",
-
     "wvu": "West Virginia",
-
     "wyu": "Wyoming",
-
     "xa ": "Christmas Island (Indian Ocean)",
-
     "xb ": "Cocos (Keeling) Islands",
-
     "xc ": "Maldives",
-
     "xd ": "Saint Kitts-Nevis",
-
     "xe ": "Marshall Islands",
-
     "xf ": "Midway Islands",
-
     "xga": "Coral Sea Islands Territory",
-
     "xh ": "Niue",
-
     "xj ": "Saint Helena",
-
     "xk ": "Saint Lucia",
-
     "xl ": "Saint Pierre and Miquelon",
-
     "xm ": "Saint Vincent and the Grenadines",
-
     "xn ": "Macedonia",
-
     "xna": "New South Wales",
-
     "xo ": "Slovakia",
-
     "xoa": "Northern Territory",
-
     "xp ": "Spratly Island",
-
     "xr ": "Czech Republic",
-
     "xra": "South Australia",
-
     "xs ": "South Georgia and the South Sandwich Islands",
-
     "xv ": "Slovenia",
-
     "xx ": "No place, unknown, or undetermined",
-
     "xxc": "Canada",
-
     "xxk": "United Kingdom",
-
     "xxu": "United States",
-
     "ye ": "Yemen",
-
     "ykc": "Yukon Territory",
-
     "za ": "Zambia",
 }
 
@@ -876,4 +543,176 @@ N_MATFORM = {
     'c': 'score',
     'w': 'web resource',
     'b': 'blu-ray'
+}
+
+NYPL_BRANCHES = {
+    "ag": "Aguilar",
+    "al": "Allerton",
+    "ba": "Baychester",
+    "bc": "Bronx Library Center",
+    "be": "Belmont",
+    "bl": "Bloomingdale",
+    "br": "George Bruce",
+    "bt": "Battery Park",
+    "ca": "Cathedral, T. C. Cooke",
+    "ch": "Chatham Square",
+    "ci": "City Island",
+    "cl": "Morningside Heights",
+    "cp": "Clason's Point",
+    "cs": "Columbus",
+    "ct": "Castle Hill",
+    "dh": "Dongan Hills",
+    "dy": "Spuyten Duyvil",
+    "ea": "Eastchester",
+    "ep": "Epiphany",
+    "ew": "Edenwald",
+    "fe": "58th Street",
+    "fw": "Fort Washington",
+    "fx": "Francis Martin",
+    "gc": "Grand Central",
+    "gd": "Grand Concourse",
+    "gk": "Great Kills",
+    "hb": "High Bridge",
+    "hd": "125th Street",
+    "hf": "Hamilton Fish Park",
+    "hg": "Hamilton Grange",
+    "hk": "Huguenot Park",
+    "hl": "Harlem",
+    "hp": "Hudson Park",
+    "hs": "Hunt's Point",
+    "ht": "Countee Cullen",
+    "hu": "115th Street (temp)",
+    "in": "Inwood",
+    "jm": "Jefferson Market",
+    "jp": "Jerome Park",
+    "kb": "Kingsbridge",
+    "kp": "Kips Bay",
+    "lb": "Andrew Heiskell",
+    "lm": "New Amsterdam",
+    "ma": "S.A. Schwarzman Bldg.",
+    "mb": "Macomb's Bridge",
+    "me": "Melrose",
+    "mh": "Mott Haven",
+    "ml": "Mulberry Street",
+    "mm": "Mid-Manhattan",
+    "mn": "Mariner's Harbor",
+    "mo": "Mosholu",
+    "mp": "Morris Park",
+    "mr": "Morrisania",
+    "mu": "Muhlenberg",
+    "my": "Performing Arts",
+    "nb": "West New Brighton",
+    "nd": "New Dorp",
+    "ns": "96th Street",
+    "ot": "Ottendorfer",
+    "pk": "Parkchester",
+    "pm": "Pelham Bay",
+    "pr": "Port Richmond",
+    "rd": "Riverdale",
+    "ri": "Roosevelt Island",
+    "rs": "Riverside",
+    "rt": "Richmondtown",
+    "sa": "St. Agnes",
+    "sb": "South Beach",
+    "sc": "Schomburg",
+    "sd": "Sedgwick",
+    "se": "Seward Park",
+    "sg": "St. George Library Center",
+    "sl": "Science, Industry & Business",
+    "ss": "67th Street",
+    "st": "Stapleton",
+    "sv": "Soundview",
+    "tg": "Throg's Neck",
+    "th": "Todt Hill-Westerleigh",
+    "tm": "Tremont",
+    "ts": "Tompkins Square",
+    "tv": "Tottenville",
+    "vc": "Van Cortlandt",
+    "vn": "Van Nest",
+    "wb": "Webster",
+    "wf": "West Farms",
+    "wh": "Washington Heights",
+    "wk": "Wakefield",
+    "wl": "Woodlawn Heights",
+    "wo": "Woodstock",
+    "wt": "Westchester Square",
+    "yv": "Yorkville",
+    "ft": "53rd Street",
+    "ls": "Library Services Center"
+}
+
+BPL_BRANCHES = {
+    "02": "Central Juv Children's Room",
+    "03": "Central YA Young Teens",
+    "04": "Central BKLYN Collection",
+    "11": "Central AMMS Art & Music",
+    "12": "Central Pop Audiovisual (Multimedia)",
+    "13": "Central HBR (Hist/Biog/Rel)",
+    "14": "Central Literature & Languages",
+    "16": "Central SST",
+    "21": "Arlington",
+    "22": "Bedford",
+    "23": "Business Library",
+    "24": "Brighton Beach",
+    "25": "Borough Park",
+    "26": "Stone Avenue",
+    "27": "Brownsville",
+    "28": "Bay Ridge",
+    "29": "Bushwick",
+    "30": "Crown Heights",
+    "31": "Carroll Gardens",
+    "32": "Coney Island",
+    "33": "Clarendon",
+    "34": "Canarsie",
+    "35": "DeKalb",
+    "36": "East Flatbush",
+    "37": "Eastern Parkway",
+    "38": "Flatbush",
+    "39": "Flatlands",
+    "40": "Fort Hamilton",
+    "41": "Greenpoint",
+    "42": "Highlawn",
+    "43": "Kensington",
+    "44": "Kings Bay",
+    "45": "Kings Highway",
+    "46": "Leonard",
+    "47": "Macon",
+    "48": "Midwood",
+    "49": "Mapleton",
+    "50": "Brooklyn Heights",
+    "51": "New Utrecht",
+    "52": "New Lots",
+    "53": "Park Slope",
+    "54": "Rugby",
+    "55": "Sunset Park",
+    "56": "Sheepshead Bay",
+    "57": "Saratoga",
+    "59": "Marcy",
+    "60": "Williamsburgh",
+    "61": "Washington Irving",
+    "62": "Walt Whitman",
+    "63": "Kidsmobile",
+    "64": "BiblioBus",
+    "65": "Cypress Hills",
+    "66": "Gerritsen Beach",
+    "67": "McKinley Park",
+    "68": "Mill Basin",
+    "69": "Pacific",
+    "70": "Red Hook",
+    "71": "Ulmer Park",
+    "72": "Bookmobile",
+    "74": "Gravesend",
+    "76": "Homecrest",
+    "77": "Windsor Terrace",
+    "78": "Paerdegat",
+    "79": "Brower Park",
+    "80": "Ryder",
+    "81": "Jamaica Bay",
+    "82": "Dyker",
+    "83": "Clinton Hill",
+    "85": "Spring Creek",
+    "87": "Cortelyou",
+    "89": "BookOps",
+    "90": "Service to the Aging SAGE/SOA",
+    "94": "Central Literacy"
 }
