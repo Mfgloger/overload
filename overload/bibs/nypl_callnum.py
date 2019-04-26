c5cce0cfd65acc3f8cd7432acba2aab36ac91787
Tomasz Kalata <klingaroo@gmail.com>
2019-04-26 15:49:04 -0400

    callnums for Sierra exports

--- overload/bibs/nypl_callnum.py
+++ overload/bibs/nypl_callnum.py
@@ -51,21 +51,78 @@ def create_nypl_fiction_callnum(
     subfields = []
     subfield_p_values = []
     field = None
-    if order_data:
-        print(order_data)
+    if order_data and order_data.fiction_location:
+        # audience element
+        if is_juvenile(audn_code) and order_data.audnType == 'j':
+            subfield_p_values.append('J')
+
+        # language element
+        if lang_prefix and order_data.wlPrefix:
+            subfield_p_values.append(lang_prefix)
+            subfields.extend(['p', ' '.join(subfield_p_values)])
+
+        if subfield_p_values:
+            subfields.extend(['p', ' '.join(subfield_p_values)])
+
+        # format element
+        if order_data.callLabel:
+            if order_data.callLabel == 'lgp':
+                subfields.extend('f', 'LG-PRINT')
+            elif order_data.callLabel == 'hol':
+                subfields.extend(['f', 'HOLIDAY'])
+            elif order_data.callLabel == 'yrd':
+                subfields.extend(['f', 'YR'])
+            elif order_data.callLabel == 'cla':
+                subfields.extend(['f', 'CLASSICS'])
+            elif order_data.callLabel == 'gra':
+                subfields.extend(['f', 'GRAPHIC'])
+            elif order_data.callLabel == 'bil':
+                subfields.extend(['f', 'BILINGUAL'])
+
+        # main content element
+        if is_picture_book(audn_code, tag_300a) and \
+                order_data.callType == 'pic':
+            subfields.extend(['a', 'PIC'])
+        elif is_picture_book(audn_code, tag_300a) and \
+                order_data.callType == 'und':  # world lang picture books
+            subfields.extend(['a', 'PIC'])
+        elif is_picture_book(audn_code, tag_300a) and \
+                order_data.callType == 'eas':
+            subfields.extend(['a', 'E'])
+        elif order_data.callType == 'fic':
+            if order_data.callLabel == 'gra':
+                subfields.extend(['a', 'GN FIC'])
+            else:
+                subfields.extend(['a', 'FIC'])
+        elif order_data.callType == 'mys':
+            subfields.extend(['a', 'MYSTERY'])
+        elif order_data.callType == 'rom':
+            subfields.extend(['a', 'ROMANCE'])
+        elif order_data.callType == 'sfn':
+            subfields.extend('a', 'SCI-FI')
+        elif order_data.callType == 'wes':
+            subfields.extend(['a', 'WESTERN'])
+        elif order_data.callType == 'urb':
+            subfields.extend(['a', 'URBAN'])
 
     else:
         if is_juvenile(audn_code):
             subfield_p_values.append('J')
+
         if lang_prefix:
             subfield_p_values.append(lang_prefix)
+
+        if subfield_p_values:
             subfields.extend(['p', ' '.join(subfield_p_values)])
+
         if is_picture_book(audn_code, tag_300a):
             subfields.extend(['a', 'PIC'])
         else:
             subfields.extend(['a', 'FIC'])
-        if cutter:
-            subfields.extend(['c', cutter])
-            field = Field(tag='091', indicators=[' ', ' '], subfields=subfields)
+
+    # cutter element
+    if cutter:
+        subfields.extend(['c', cutter])
+        field = Field(tag='091', indicators=[' ', ' '], subfields=subfields)
 
     return field
