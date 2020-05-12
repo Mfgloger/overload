10b10bed432c970b3ddee2cea9b258d172e9ad5d
klinga <klingaroo@gmail.com>
2020-05-11 23:08:23 -0400

    pyinstalled bumped to 3.6
    due to security issue

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ from setuptools import setup, find_packages
 
 
 # Package meta-data.
-NAME = 'overload'
-DESCRIPTION = 'BookOps toolbox.'
-URL = 'https://github.com/BookOps/overload'
-EMAIL = 'klingaroo@gmail.com'
-AUTHOR = 'Tomek Kalata'
-REQUIRES_PYTHON = '>=2.7.13'
+NAME = "overload"
+DESCRIPTION = "BookOps toolbox."
+URL = "https://github.com/BookOps/overload"
+EMAIL = "klingaroo@gmail.com"
+AUTHOR = "Tomek Kalata"
+REQUIRES_PYTHON = ">=2.7.13"
 VERSION = None
 
 # Requiared packages
@@ -44,7 +44,7 @@ REQUIRED = [
     "pyasn1==0.4.4",
     "pyasn1-modules==0.2.2",
     "pycryptodome==3.6.6",
-    "PyInstaller==3.4",
+    "PyInstaller==3.6",
     "pymarc==3.1.7",
     "pypiwin32==223",
     "python-dateutil==2.6.1",
@@ -61,16 +61,16 @@ REQUIRED = [
     "SQLAlchemy==1.3.8",
     "unidecode==1.0.23",
     "uritemplate==3.0.0",
-    "urllib3==1.24.2"
+    "urllib3==1.24.2",
 ]
 
 # requires manual installation of Aaron Lav's PyZ3950
 # see details here: http://www.panix.com/~asl2/software/PyZ3950/
 
-with open('README.md') as f:
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
 setup(
@@ -78,26 +78,26 @@ setup(
     version=VERSION,
     description=DESCRIPTION,
     long_description=readme,
-    author='Tomasz Kalata',
+    author="Tomasz Kalata",
     author_email=EMAIL,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     install_requires=REQUIRED,
     include_package_data=True,
     license=license,
     classifiers=[
         # Trove classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7'
-    ]
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
 )
 
 here = os.path.abspath(os.path.dirname(__file__))
 
 about = {}
 if not VERSION:
-    with open(os.path.join(here, NAME, '__version__.py')) as f:
+    with open(os.path.join(here, NAME, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about["__version__"] = VERSION
