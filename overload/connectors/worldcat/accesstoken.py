eb384156aa9c5caad007c7e24ac5cf7f340d2346
klinga <klingaroo@gmail.com>
2020-05-13 20:18:53 -0400

    blaken

--- overload/connectors/worldcat/accesstoken.py
+++ overload/connectors/worldcat/accesstoken.py
@@ -117,19 +117,15 @@ class AccessToken(object):
     wskey = None
 
     valid_options = [
-        'scope',
-        'authenticating_institution_id',
-        'context_institution_id',
-        'redirect_uri',
-        'code',
-        'refresh_token'
+        "scope",
+        "authenticating_institution_id",
+        "context_institution_id",
+        "redirect_uri",
+        "code",
+        "refresh_token",
     ]
 
-    validGrantTypes = [
-        'authorization_code',
-        'refresh_token',
-        'client_credentials'
-    ]
+    validGrantTypes = ["authorization_code", "refresh_token", "client_credentials"]
 
     def __init__(self, authorization_server, grant_type=None, options=None):
         """Constructor.
@@ -150,39 +146,51 @@ class AccessToken(object):
         """
         self.authorization_server = authorization_server
         if grant_type is None or grant_type not in AccessToken.validGrantTypes:
-            raise InvalidGrantType('You must pass a valid grant type to construct an Access Token.')
+            raise InvalidGrantType(
+                "You must pass a valid grant type to construct an Access Token."
+            )
         self.grant_type = grant_type
 
         if not options:
-            raise NoOptionsPassed('You must pass at least one option to construct an Access Token. Valid options '
-                                  'are scope, authenticating_institution_id, context_institution_id, redirect_uri, '
-                                  'code and refresh_token')
+            raise NoOptionsPassed(
+                "You must pass at least one option to construct an Access Token. Valid options "
+                "are scope, authenticating_institution_id, context_institution_id, redirect_uri, "
+                "code and refresh_token"
+            )
 
-        if self.grant_type == 'authorization_code' and (
-                            'code' not in options or
-                            'redirect_uri' not in options or
-                            'authenticating_institution_id' not in options or
-                            'context_institution_id' not in options):
-            raise RequiredOptionsMissing('You must pass the options: code, redirect_uri, '
-                                         'authenticating_institution_id and context_institution_id to construct an Access '
-                                         'Token using the authorization_code grant type.')
-
-        elif self.grant_type == 'client_credentials' and (
-                            not 'scope' in options or
-                            not 'authenticating_institution_id' in options or
-                            not 'context_institution_id' in options or
-                            not 'scope' in options):
+        if self.grant_type == "authorization_code" and (
+            "code" not in options
+            or "redirect_uri" not in options
+            or "authenticating_institution_id" not in options
+            or "context_institution_id" not in options
+        ):
             raise RequiredOptionsMissing(
-                'You must pass the options: scope, authenticating_institution_id and context_institution_id ' +
-                'to construct an Access Token using the client_credential grant type.')
+                "You must pass the options: code, redirect_uri, "
+                "authenticating_institution_id and context_institution_id to construct an Access "
+                "Token using the authorization_code grant type."
+            )
 
-        elif self.grant_type == 'refresh_token' and 'refresh_token' not in options:
+        elif self.grant_type == "client_credentials" and (
+            not "scope" in options
+            or not "authenticating_institution_id" in options
+            or not "context_institution_id" in options
+            or not "scope" in options
+        ):
             raise RequiredOptionsMissing(
-                'You must pass the option refresh_token to construct an Access Token using the ' +
-                'refresh_token grant type.')
+                "You must pass the options: scope, authenticating_institution_id and context_institution_id "
+                + "to construct an Access Token using the client_credential grant type."
+            )
 
-        if 'scope' in options and not isinstance(options['scope'], list):
-            raise RequiredOptionsMissing("scope must be a list of one or more scopes, i.e. ['WMS_NCIP' {, ...}]")
+        elif self.grant_type == "refresh_token" and "refresh_token" not in options:
+            raise RequiredOptionsMissing(
+                "You must pass the option refresh_token to construct an Access Token using the "
+                + "refresh_token grant type."
+            )
+
+        if "scope" in options and not isinstance(options["scope"], list):
+            raise RequiredOptionsMissing(
+                "scope must be a list of one or more scopes, i.e. ['WMS_NCIP' {, ...}]"
+            )
 
         for key, value in six.iteritems(options):
             if key in AccessToken.valid_options:
@@ -193,36 +201,39 @@ class AccessToken(object):
     def is_expired(self):
         """Test if the token is expired. Returns true if it is."""
         status = False
-        if time.mktime(time.strptime(
-                self.expires_at, "%Y-%m-%d %H:%M:%SZ")) < time.time():
+        if (
+            time.mktime(time.strptime(self.expires_at, "%Y-%m-%d %H:%M:%SZ"))
+            < time.time()
+        ):
             status = True
         return status
 
     def create(self, wskey, user=None):
         """Create an access token."""
-        if not wskey.__class__.__name__ == 'Wskey':
-            raise InvalidObject('A valid Wskey object is required.')
-        elif user is not None and not user.__class__.__name__ == 'User':
-            raise InvalidObject('A valid User object is required.')
+        if not wskey.__class__.__name__ == "Wskey":
+            raise InvalidObject("A valid Wskey object is required.")
+        elif user is not None and not user.__class__.__name__ == "User":
+            raise InvalidObject("A valid User object is required.")
         self.wskey = wskey
         if user is not None:
             self.user = user
-            self.options = {'user': self.user}
+            self.options = {"user": self.user}
         authorization = self.wskey.get_hmac_signature(
-            method='POST',
-            request_url=self.access_token_url,
-            options=self.options)
+            method="POST", request_url=self.access_token_url, options=self.options
+        )
         print(authorization)
         self.request_access_token(authorization, self.access_token_url)
 
     def refresh(self):
         """Refresh an access token."""
         if self.wskey is None:
-            raise InvalidObject('AccessToken must have an associated WSKey Property')
+            raise InvalidObject("AccessToken must have an associated WSKey Property")
 
-        self.grant_type = 'refresh_token'
+        self.grant_type = "refresh_token"
         self.access_token_url = self.get_access_token_url()
-        authorization = self.wskey.get_hmac_signature(method='POST', request_url=self.access_token_url)
+        authorization = self.wskey.get_hmac_signature(
+            method="POST", request_url=self.access_token_url
+        )
         self.request_access_token(authorization, self.access_token_url)
 
     def request_access_token(self, authorization, url):
@@ -230,11 +241,9 @@ class AccessToken(object):
 
         s = Session()
         data = {}
-        headers = {
-            'Authorization': authorization,
-            'Accept': 'application/json'}
+        headers = {"Authorization": authorization, "Accept": "application/json"}
 
-        req = Request('POST', url, data=data, headers=headers)
+        req = Request("POST", url, data=data, headers=headers)
         prepped = req.prepare()
         print(prepped.url)
 
@@ -260,22 +269,36 @@ class AccessToken(object):
 
     def get_access_token_url(self):
         """ get Access Token URL """
-        access_token_url = self.authorization_server + '/accessToken?grant_type=' + self.grant_type
-        if self.grant_type == 'refresh_token':
-            access_token_url += '&refresh_token=' + self.refresh_token.refresh_token
-        elif self.grant_type == 'authorization_code':
+        access_token_url = (
+            self.authorization_server + "/accessToken?grant_type=" + self.grant_type
+        )
+        if self.grant_type == "refresh_token":
+            access_token_url += "&refresh_token=" + self.refresh_token.refresh_token
+        elif self.grant_type == "authorization_code":
             access_token_url += (
-                '&' + 'code=' + self.code +
-                '&' + 'authenticatingInstitutionId=' + self.authenticating_institution_id +
-                '&' + 'contextInstitutionId=' + self.context_institution_id +
-                '&' + six.moves.urllib.parse.urlencode({'redirect_uri': self.redirect_uri}))
-        elif self.grant_type == 'client_credentials':
+                "&"
+                + "code="
+                + self.code
+                + "&"
+                + "authenticatingInstitutionId="
+                + self.authenticating_institution_id
+                + "&"
+                + "contextInstitutionId="
+                + self.context_institution_id
+                + "&"
+                + six.moves.urllib.parse.urlencode({"redirect_uri": self.redirect_uri})
+            )
+        elif self.grant_type == "client_credentials":
             access_token_url += (
-                '&authenticatingInstitutionId=' + self.authenticating_institution_id +
-                '&contextInstitutionId=' + self.context_institution_id +
-                '&scope=' + '%20'.join(self.scope))
+                "&authenticatingInstitutionId="
+                + self.authenticating_institution_id
+                + "&contextInstitutionId="
+                + self.context_institution_id
+                + "&scope="
+                + "%20".join(self.scope)
+            )
         else:
-            access_token_url = ''
+            access_token_url = ""
 
         return access_token_url
 
@@ -287,51 +310,61 @@ class AccessToken(object):
         try:
             response_json = json.loads(response_string)
         except ValueError:
-            print("ValueError: Unable to decode this Access Token response string to JSON:")
+            print(
+                "ValueError: Unable to decode this Access Token response string to JSON:"
+            )
             print(response_string)
             return
 
-        self.access_token_string = response_json.get('access_token', None)
-        self.type = response_json.get('token_type', None)
-        self.expires_at = response_json.get('expires_at', None)
-        self.expires_in = response_json.get('expires_in', None)
-        self.context_institution_id = response_json.get('context_institution_id', None)
-        self.error_code = response_json.get('error_code', None)
+        self.access_token_string = response_json.get("access_token", None)
+        self.type = response_json.get("token_type", None)
+        self.expires_at = response_json.get("expires_at", None)
+        self.expires_in = response_json.get("expires_in", None)
+        self.context_institution_id = response_json.get("context_institution_id", None)
+        self.error_code = response_json.get("error_code", None)
 
-        principal_id = response_json.get('principalID', None)
-        principal_idns = response_json.get('principalIDNS', None)
+        principal_id = response_json.get("principalID", None)
+        principal_idns = response_json.get("principalIDNS", None)
 
-        if principal_id is not None and principal_idns is not None and not principal_id == '' and not principal_idns == '':
+        if (
+            principal_id is not None
+            and principal_idns is not None
+            and not principal_id == ""
+            and not principal_idns == ""
+        ):
             self.user = User(
                 authenticating_institution_id=self.authenticating_institution_id,
                 principal_id=principal_id,
-                principal_idns=principal_idns
+                principal_idns=principal_idns,
             )
 
-        refresh_token = response_json.get('refresh_token', None)
+        refresh_token = response_json.get("refresh_token", None)
 
         if refresh_token is not None:
             self.refresh_token = RefreshToken(
                 tokenValue=refresh_token,
-                expires_in=response_json.get('refresh_token_expires_in', None),
-                expires_at=response_json.get('refresh_token_expires_at', None)
+                expires_in=response_json.get("refresh_token_expires_in", None),
+                expires_at=response_json.get("refresh_token_expires_at", None),
             )
 
     def parse_error_response(self, http_error):
         try:
             error_json = json.loads(http_error.read())
             self.error_code = http_error.getcode()
-            self.error_message = error_json['message']
-            self.error_detail = error_json['details']
+            self.error_message = error_json["message"]
+            self.error_detail = error_json["details"]
             self.error_url = http_error.geturl()
         except ValueError:
-            print("ValueError: Unable to decode this Access Token response string to JSON:")
+            print(
+                "ValueError: Unable to decode this Access Token response string to JSON:"
+            )
             print(response_string)
             return
 
     def __str__(self):
 
-        return string.Template("""
+        return string.Template(
+            """
 access_token_url: $access_token_url
 
 access_token_string             $access_token_string
@@ -354,26 +387,29 @@ type:                           $type
 user:
 $user
 wskey:
-$wskey""").substitute({
-            'access_token_url': self.access_token_url.
-                replace('?', '?\n' + ' ' * 18).
-                replace('&', '\n' + ' ' * 18 + '&'),
-            'access_token_string':self.access_token_string,
-            'authenticating_institution_id': self.authenticating_institution_id,
-            'authorization_server': self.authorization_server,
-            'code': self.code,
-            'context_institution_id': self.context_institution_id,
-            'error_code': self.error_code,
-            'error_message': self.error_message,
-            'error_url': self.error_url,
-            'expires_at': self.expires_at,
-            'expires_in': self.expires_in,
-            'grant_type': self.grant_type,
-            'options': self.options,
-            'redirect_uri': self.redirect_uri,
-            'refresh_token': self.refresh_token,
-            'scope': self.scope,
-            'type': self.type,
-            'user': self.user,
-            'wskey': self.wskey
-        })
+$wskey"""
+        ).substitute(
+            {
+                "access_token_url": self.access_token_url.replace(
+                    "?", "?\n" + " " * 18
+                ).replace("&", "\n" + " " * 18 + "&"),
+                "access_token_string": self.access_token_string,
+                "authenticating_institution_id": self.authenticating_institution_id,
+                "authorization_server": self.authorization_server,
+                "code": self.code,
+                "context_institution_id": self.context_institution_id,
+                "error_code": self.error_code,
+                "error_message": self.error_message,
+                "error_url": self.error_url,
+                "expires_at": self.expires_at,
+                "expires_in": self.expires_in,
+                "grant_type": self.grant_type,
+                "options": self.options,
+                "redirect_uri": self.redirect_uri,
+                "refresh_token": self.refresh_token,
+                "scope": self.scope,
+                "type": self.type,
+                "user": self.user,
+                "wskey": self.wskey,
+            }
+        )
