85323dcb832389c81924c233fe0c818c457cb252
klinga <klingaroo@gmail.com>
2020-05-17 00:39:35 -0400

    blackened

--- tests/test_bib_parsers.py
+++ tests/test_bib_parsers.py
@@ -10,16 +10,16 @@ class TestUtils(unittest.TestCase):
     """Test utilities functions used in Overload"""
 
     def test_parse_isbn_10_digits_only(self):
-        self.assertIsNotNone(parsers.parse_isbn('83-922033-1-3'))
+        self.assertIsNotNone(parsers.parse_isbn("83-922033-1-3"))
 
     def test_parse_isbn_13_digit_only(self):
-        self.assertIsNotNone(parsers.parse_isbn('9788374147323'))
+        self.assertIsNotNone(parsers.parse_isbn("9788374147323"))
 
     def test_parse_isbn_10_digit_x(self):
-        self.assertIsNotNone(parsers.parse_isbn('061543326X'))
+        self.assertIsNotNone(parsers.parse_isbn("061543326X"))
 
     def test_parse_isbn_13_digit_x(self):
-        self.assertIsNotNone(parsers.parse_isbn('978141049620x (hardcover)'))
+        self.assertIsNotNone(parsers.parse_isbn("978141049620x (hardcover)"))
 
     # def test_parse_incorrect_isbn(self):
     #     # make corrections to isbn parser
@@ -33,62 +33,47 @@ class TestParseUPC(unittest.TestCase):
     """Test parsing identifier found in 024$a MARC fieled"""
 
     def test_parsing_good_UPC(self):
-        self.assertEqual(
-            parsers.parse_upc('6706878182'),
-            '6706878182')
+        self.assertEqual(parsers.parse_upc("6706878182"), "6706878182")
 
     def test_parsing_UPC_with_price(self):
-        self.assertEqual(
-            parsers.parse_upc('8616227633 : $19.98'),
-            '8616227633')
+        self.assertEqual(parsers.parse_upc("8616227633 : $19.98"), "8616227633")
 
     def test_parsing_alphanumberic_UPC(self):
-        self.assertEqual(
-            parsers.parse_upc('M215104174'),
-            'M215104174')
+        self.assertEqual(parsers.parse_upc("M215104174"), "M215104174")
 
 
 class TestParseISSN(unittest.TestCase):
     """Test parsing ISSN found in 022$a MARC fieled"""
 
     def test_parsing_good_digit_only_ISSN(self):
-        self.assertEqual(
-            parsers.parse_issn('0378-5955'),
-            '03785955')
+        self.assertEqual(parsers.parse_issn("0378-5955"), "03785955")
 
     def test_parsing_good_digit_x_ISSN(self):
-        self.assertEqual(
-            parsers.parse_issn('2434-561X'),
-            '2434561X')
+        self.assertEqual(parsers.parse_issn("2434-561X"), "2434561X")
 
     def test_parsing_incorrect_ISSN(self):
-        self.assertIsNone(
-            parsers.parse_issn('M215104174'))
+        self.assertIsNone(parsers.parse_issn("M215104174"))
 
 
 class TestParseSierraID(unittest.TestCase):
     """Test parsing Sierra bib and order id numbers"""
 
     def test_bib_number_parsing(self):
-        self.assertIsNotNone(bibs.parse_sierra_id('.b119629811'))
-        self.assertEqual(
-            bibs.parse_sierra_id('.b119629811'),
-            '11962981')
+        self.assertIsNotNone(bibs.parse_sierra_id(".b119629811"))
+        self.assertEqual(bibs.parse_sierra_id(".b119629811"), "11962981")
 
     def test_order_number_parsing(self):
-        self.assertIsNotNone(bibs.parse_sierra_id('.o16799069'))
-        self.assertEqual(
-            bibs.parse_sierra_id('.o16799069'),
-            '1679906')
+        self.assertIsNotNone(bibs.parse_sierra_id(".o16799069"))
+        self.assertEqual(bibs.parse_sierra_id(".o16799069"), "1679906")
 
     def test_wrong_id_number_parsing(self):
-        self.assertIsNone(bibs.parse_sierra_id('349876789087'))
+        self.assertIsNone(bibs.parse_sierra_id("349876789087"))
 
     def test_None_id_parsing(self):
         self.assertIsNone(bibs.parse_sierra_id(None))
 
     def test_empty_string_parsing(self):
-        self.assertIsNone(bibs.parse_sierra_id(''))
+        self.assertIsNone(bibs.parse_sierra_id(""))
 
 
 class TestExtractRecordEncoding(unittest.TestCase):
@@ -98,14 +83,12 @@ class TestExtractRecordEncoding(unittest.TestCase):
         self.assertIsNone(parsers.extract_record_encoding())
 
     def test_leader_utf8_endcoding(self):
-        leader = '01451nam a2200397 i 4500'
-        self.assertEqual(
-            parsers.extract_record_encoding(leader), 'a')
+        leader = "01451nam a2200397 i 4500"
+        self.assertEqual(parsers.extract_record_encoding(leader), "a")
 
     def test_leader_marc8_encodign(self):
-        leader = '01451nam  2200397 i 4500'
-        self.assertEqual(
-            parsers.extract_record_encoding(leader), ' ')
+        leader = "01451nam  2200397 i 4500"
+        self.assertEqual(parsers.extract_record_encoding(leader), " ")
 
 
 class TestExtractRecordLvl(unittest.TestCase):
@@ -115,14 +98,12 @@ class TestExtractRecordLvl(unittest.TestCase):
         self.assertIsNone(parsers.extract_record_lvl())
 
     def test_leader_full_lvl(self):
-        leader = '01451nam  2200397 i 4500'
-        self.assertEqual(
-            parsers.extract_record_lvl(leader), ' ')
+        leader = "01451nam  2200397 i 4500"
+        self.assertEqual(parsers.extract_record_lvl(leader), " ")
 
     def test_leader_abbreviated_lvl(self):
-        leader = '01451nam  22003973i 4500'
-        self.assertEqual(
-            parsers.extract_record_lvl(leader), '3')
+        leader = "01451nam  22003973i 4500"
+        self.assertEqual(parsers.extract_record_lvl(leader), "3")
 
 
 class TestExtractRecordType(unittest.TestCase):
@@ -132,14 +113,12 @@ class TestExtractRecordType(unittest.TestCase):
         self.assertIsNone(parsers.extract_record_type())
 
     def test_leader_record_type_is_lang_material(self):
-        leader = '01451nam  2200397 i 4500'
-        self.assertEqual(
-            parsers.extract_record_type(leader), 'a')
+        leader = "01451nam  2200397 i 4500"
+        self.assertEqual(parsers.extract_record_type(leader), "a")
 
     def test_leader_record_type_is_projected_medium(self):
-        leader = '01451ngm  22003973i 4500'
-        self.assertEqual(
-            parsers.extract_record_type(leader), 'g')
+        leader = "01451ngm  22003973i 4500"
+        self.assertEqual(parsers.extract_record_type(leader), "g")
 
 
 class TestExtractAudienceCode(unittest.TestCase):
@@ -147,32 +126,30 @@ class TestExtractAudienceCode(unittest.TestCase):
 
     def test_no_record_leader(self):
         leader = None
-        t008 = r'190911s2019\\\\caua\\\\\\\\\\000\1\eng\d'
+        t008 = r"190911s2019\\\\caua\\\\\\\\\\000\1\eng\d"
         self.assertIsNone(parsers.get_audience_code(leader, t008))
 
     def test_no_008_tag(self):
-        leader = '01451nam  2200397 i 4500'
+        leader = "01451nam  2200397 i 4500"
         t008 = None
         self.assertIsNone(parsers.get_audience_code(leader, t008))
 
     def test_audn_code_for_lang_material(self):
-        leader = '01451nam  2200397 i 4500'
-        t008 = r'190911s2019    caua          000 1 eng d'
-        self.assertEqual(
-            parsers.get_audience_code(leader, t008), ' ')
+        leader = "01451nam  2200397 i 4500"
+        t008 = r"190911s2019    caua          000 1 eng d"
+        self.assertEqual(parsers.get_audience_code(leader, t008), " ")
 
     def test_audn_code_for_projected_medium(self):
-        leader = '01451ngm  22003973i 4500'
-        t008 = r'190916p20192018xxu156            vleng d'
+        leader = "01451ngm  22003973i 4500"
+        t008 = r"190916p20192018xxu156            vleng d"
         self.assertIsNone(parsers.get_audience_code(leader, t008))
 
 
 class TestGetLanguageCode(unittest.TestCase):
-        """Tests extraction of language code from 008 tag"""
+    """Tests extraction of language code from 008 tag"""
 
     def test_no_008_tag(self):
         self.assertIsNone(parsers.get_language_code(None))
-
 
 
 class TestParseLangPrefix(unittest.TestCase):
@@ -182,6 +159,5 @@ class TestParseLangPrefix(unittest.TestCase):
         pass
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
