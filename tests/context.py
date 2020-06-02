aa29c134c58fb89a301e5ef13ca43ffb3b3d4e7d
klinga <klingaroo@gmail.com>
2020-06-01 23:28:28 -0400

    callnum imports

--- tests/context.py
+++ tests/context.py
@@ -32,12 +32,15 @@ from overload.bibs.nypl_callnum import (
 )
 from overload.bibs.bpl_callnum import (
     create_bpl_callnum,
-    determine_cutter,
-    determine_biographee_name,
     has_division_conflict,
     is_adult_division,
+)
+from overload.bibs.callnum import (
+    determine_cutter,
+    determine_biographee_name,
     valid_audience,
 )
+from overload.bibs import xml_bibs
 from overload.pvf import vendors
 from overload.pvf.analyzer import PVRReport, PVR_NYPLReport
 from overload.pvf import reports
